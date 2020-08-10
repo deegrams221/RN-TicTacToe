792229548b084a6fad44b2641d6832fb4dd2a024
Diana Grams <deegrams221@gmail.com>
2020-08-10 15:57:29 -0600

    working on tutorial - getting error tho

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+import {
+  Body,
+  Button,
+  Card,
+  CardItem,
+  Container,
+  Content,
+  Header,
+  Icon,
+  Left,
+  Right,
+  Text,
+  Thumbnail,
+} from 'native-base';
+import React, { Component } from 'react';
+import { Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>checking app</Text>
-      <StatusBar style='auto' />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <Expo.AppLoading />;
+    }
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail
+                  source={{ uri: 'https://unsplash.com/photos/9BpoC5PxPlE' }}
+                />
+                <Body>
+                  <Text>Diana Grams</Text>
+                  <Text note>Web Dev / Artist / Cosplayer</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image
+                source={{ uri: 'https://unsplash.com/photos/9BpoC5PxPlE' }}
+                style={{ height: 200, width: null, flex: 1 }}
+              />
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name='thumbs-up' />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name='chatbubbles' />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>11h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
