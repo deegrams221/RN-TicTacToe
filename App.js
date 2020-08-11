ccbc2b639662bc1aa8bc34d4b60414a26bfef575
Diana Grams <deegrams221@gmail.com>
2020-08-11 16:54:34 -0600

    native-base example

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import Expo from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 import {
   Body,
   Button,
@@ -16,26 +18,28 @@ import {
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 
-export default class App extends Component {
+export default class CardImageExample extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true,
+      isReady: false,
     };
   }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
+  async componentDidMount() {
+    await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
     });
-    this.setState({ isLoading: false });
+    this.setState({ isReady: true });
   }
 
   render() {
-    if (this.state.isLoading) {
-      return <Expo.AppLoading />;
+    if (!this.state.isReady) {
+      return <AppLoading />;
     }
+
     return (
       <Container>
         <Header />
@@ -44,18 +48,24 @@ export default class App extends Component {
             <CardItem>
               <Left>
                 <Thumbnail
-                  source={{ uri: 'https://unsplash.com/photos/9BpoC5PxPlE' }}
+                  source={{
+                    uri:
+                      'https://images.unsplash.com/photo-1596432150438-3ef028cca147?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+                  }}
                 />
                 <Body>
                   <Text>Diana Grams</Text>
-                  <Text note>Web Dev / Artist / Cosplayer</Text>
+                  <Text note>Web Dev | Veteran | Artist</Text>
                 </Body>
               </Left>
             </CardItem>
             <CardItem cardBody>
               <Image
-                source={{ uri: 'https://unsplash.com/photos/9BpoC5PxPlE' }}
-                style={{ height: 200, width: null, flex: 1 }}
+                source={{
+                  uri:
+                    'https://images.unsplash.com/photo-1580854679078-58401581d98b?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+                }}
+                style={{ height: 300, width: null, flex: 1 }}
               />
             </CardItem>
             <CardItem>
